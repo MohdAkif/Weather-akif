360811f60ad00ec242bfc68e3e1c13b6ca26282a
mohd akif <mohdakif00000@gmail.com>
2021-08-20 01:02:22 +0530

    my first commit using SSH

---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+const submitBtn=document.getElementById("submitBtn");
+const cityName=document.getElementById("cityName");
+const city_name=document.getElementById("city_name");
+const temp_status=document.getElementById("temp_status");
+const temp=document.getElementById("temp");
+const data_hide=document.querySelector('.middle_layer');
+const getInfo = async (event) =>{
+    event.preventDefault();
+    if(cityName.value===""){
+        city_name.innerText="Please Enter the City Name";
+        data_hide.classList.add('data_hide');
+    }
+    else{
+        try{
+            let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&units=metric&appid=cba636b4a10641fbb0fd2835b7db36a7`;
+            const response = await fetch(url);
+            const data=await response.json();
+            const arrData=[data];
+            city_name.innerText=`${arrData[0].name},${arrData[0].sys.country}`;
+            temp.innerHTML=`${arrData[0].main.temp}<sup>o</sup>C`;
+            tempMood=arrData[0].weather[0].main;
+            //console.log(data);
+            //console.log(tempMood);
+            if (tempMood == "Clear") {
+                temp_status.innerHTML =
+                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempMood == "Clouds") {
+                temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+            } else if (tempMood == "Rain") {
+                temp_status.innerHTML =
+                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else {
+                temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+            }
+            data_hide.classList.remove('data_hide');
+        }
+        catch {
+            city_name.innerText="Please Enter the valid city Name";
+            data_hide.classList.add('data_hide');
+        }
+    }
+}
+submitBtn.addEventListener("click",getInfo);
